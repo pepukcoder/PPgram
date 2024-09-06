5727570e510e2d1df6398e4b9812aad5054f4efd
Pepukcoder <pepukcoder@gmail.com>
2024-09-05 23:36:23 -0700

    Initial project

---
+++ PPgram/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PPgram.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ PPgram/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PPgram.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
