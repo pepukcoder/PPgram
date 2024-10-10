7ade6eec8416d70ec2a4b0249fe3319dbd0c62e0
Pepukcoder <pepukcoder@gmail.com>
2024-10-10 21:48:53 +0600

    Align chat hstory to bottom

--- PPgram/MVVM/ViewModels/ChatViewModel.cs
+++ PPgram/MVVM/ViewModels/ChatViewModel.cs
@@ -20,7 +20,7 @@ partial class ChatViewModel : ViewModelBase
     private bool _rightGridVisible;
     public ChatViewModel()
     {
-        RightGridVisible = true;
+        RightGridVisible = false;
         ChatList.Add(new ChatModel
         {
             Name = "Pepuk",
