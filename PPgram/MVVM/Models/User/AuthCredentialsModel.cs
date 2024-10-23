5b96c236b38279603e1163bd5d14d5689a2534e1
NoFC <135505573+pepukcoder@users.noreply.github.com>
2024-10-23 23:16:53 +0600

    Update AuthCredentialsModel.cs

--- PPgram/MVVM/Models/User/AuthCredentialsModel.cs
+++ PPgram/MVVM/Models/User/AuthCredentialsModel.cs
@@ -1,6 +1,6 @@
 // Basic Model for Authentication
 // Required to store the provided by the API UserId and SessionId in a json file 
-public class AuthCredentialsModel
+internal class AuthCredentialsModel
 {
     public int UserId { get; set; }
     public required string SessionId { get; set; }
