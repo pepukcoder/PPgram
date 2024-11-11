7ae28eac314f3c3c7ad17c311ce64475d970a835
NoFC <pepukcoder@gmail.com>
2024-11-11 13:42:22 +0600

    add badge insertion to new messages

--- PPgram/Helpers/MessageChainManager.cs
+++ PPgram/Helpers/MessageChainManager.cs
@@ -5,6 +5,7 @@ using PPgram.MVVM.Models.User;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Claims;
 
 namespace PPgram.Helpers;
 
@@ -40,6 +41,7 @@ internal class MessageChainManager
         var message = chat.OfType<MessageModel>().LastOrDefault();
         if (message == null) return;
         SetRole(message, chat);
+        SetBadge(message, chat);
     }
     private void SetRole(MessageModel message, ObservableCollection<ChatItem> chat)
     {
@@ -72,5 +74,17 @@ internal class MessageChainManager
             }
             message.Status = Shared.MessageStatus.None;
         }
+    }
+    private void SetBadge(MessageModel message, ObservableCollection<ChatItem> chat)
+    {
+        int currentIndex = chat.IndexOf(message);
+        if (currentIndex > 0)
+        {
+            ChatItem previous = chat[currentIndex - 1];
+            if (previous is DateBadgeModel) return;
+            if (previous is MessageModel prevm 
+                && DateTimeOffset.FromUnixTimeSeconds(message.Time).Date == DateTimeOffset.FromUnixTimeSeconds(prevm.Time).Date) return;
+        }
+        chat.Insert(currentIndex, new DateBadgeModel() { Date = message.Time });
     }
 }
