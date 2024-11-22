e3cfdaaeaedc2160c8ab234c58577502799fb449
NoFC <pepukcoder@gmail.com>
2024-11-22 22:48:47 +0600

    added file picker window

--- PPgram/MVVM/Views/ChatView.axaml.cs
+++ PPgram/MVVM/Views/ChatView.axaml.cs
@@ -2,9 +2,11 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using Avalonia.Xaml.Interactions.Custom;
 using PPgram.MVVM.ViewModels;
 using System.Diagnostics;
+using System.IO;
 
 namespace PPgram.MVVM.Views;
 
@@ -13,10 +15,25 @@ public partial class ChatView : UserControl
     public ChatView()
     {
         InitializeComponent();
-        MessageHistory.AddHandler(PointerPressedEvent, ShowHistoryFlyout, RoutingStrategies.Tunnel);
+        MessageHistory.AddHandler(PointerPressedEvent, ShowFlyout, RoutingStrategies.Tunnel);
+        AttachButton.AddHandler(PointerPressedEvent, OpenFileDialog, RoutingStrategies.Tunnel);
     }
-    private void ShowHistoryFlyout(object? sender, PointerPressedEventArgs e)
+    private void ShowFlyout(object? sender, PointerPressedEventArgs e)
     {
         if (sender is Control c) Flyout.ShowAttachedFlyout(c);
+    }
+    private async void OpenFileDialog(object? sender, RoutedEventArgs args)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        if (topLevel == null) return;
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Select files",
+            AllowMultiple = true
+        });
+        if (files.Count >= 1 && this.DataContext is ChatViewModel chatViewModel)
+        {
+            // send opened file paths to viewmodel
+        }
     }
 }
