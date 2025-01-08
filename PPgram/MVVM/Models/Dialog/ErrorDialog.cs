23573e2812691ae5ebef2e7b8293c712532200b4
NoFC <pepukcoder@gmail.com>
2025-01-08 21:28:42 +0600

    added error dialog

---
+++ PPgram/MVVM/Models/Dialog/ErrorDialog.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Layout;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PPgram.MVVM.Models.Dialog;
+
+internal partial class ErrorDialog : Dialog
+{
+    [ObservableProperty]
+    private string text = string.Empty;
+    public ErrorDialog()
+    {
+        Position = VerticalAlignment.Top;
+        backpanel = false;
+    }
+}
